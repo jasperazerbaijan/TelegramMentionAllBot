315c6be01ec9600fe8d2532d81f2354f937a4bd2
eduardo.ferro <eduardo.ferro@udc.es>
2019-07-01 18:58:59 +0200

    base of application

---
+++ main.py
@@ -0,0 +1,27 @@
+from telegram.ext import Updater, CommandHandler
+
+members = {}
+
+
+
+def all(bot, update):
+    update.message.reply_text(
+        'HEY {}'.format(members[str(update.message.chat.id)]))
+
+def set_members(bot, update):
+    global members 
+    members[str(update.message.chat.id)]=update.message.text.replace("/set_members"," ")
+    update.message.reply_text(
+        'Guardados miembros')
+    print ("Setted members: "+update.message.text)
+    
+
+updater = Updater('KEY')
+
+
+updater.dispatcher.add_handler(CommandHandler('all', all))
+
+updater.dispatcher.add_handler(CommandHandler('set_members', set_members))
+
+updater.start_polling()
+updater.idle()
